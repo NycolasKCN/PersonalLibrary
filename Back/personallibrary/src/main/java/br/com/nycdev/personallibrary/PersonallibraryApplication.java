517e9b1ac9bb52de023a638dc53a365d740073f1
Nycolas Kevin <nycolas.costa@dcx.ufpb.br>
2023-04-27 18:44:55 -0300

    uploading base project

---
+++ Back/personallibrary/src/main/java/br/com/nycdev/personallibrary/PersonallibraryApplication.java
@@ -0,0 +1,13 @@
+package br.com.nycdev.personallibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonallibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonallibraryApplication.class, args);
+	}
+
+}
